7fdee86deae5065e996459b6642a7ef3d554c67b
sakshi <sakshikolekar2003@gmail.com>
2024-01-05 14:57:59 +0530

    dat object and its methods

---
+++ dates.js
@@ -0,0 +1,9 @@
+const date=new Date()
+console.log(date);
+console.log(date.toLocaleString());
+console.log(date.toISOString());
+console.log(date.toJSON());
+console.log(date.getFullYear());
+console.log(date.getMilliseconds());
+
+console.log(date.toLocaleDateString());
