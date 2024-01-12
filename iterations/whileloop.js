94bbea2a2c7f27c2540a2fad34f940e3c22a3975
sakshi <sakshikolekar2003@gmail.com>
2024-01-12 19:57:54 +0530

    while and do while loop in js..

---
+++ iterations/whileloop.js
@@ -0,0 +1,26 @@
+let index=0
+while (index<=10) {
+    console.log(`value of index is  ${index}`);
+    index+=2
+}
+
+let myArray=["abc","bcd","efg"]
+let arr=0 //initialization
+while (arr<myArray.length) {//conditional
+    
+    console.log(`value of ${myArray[arr]} `);
+    arr++//operation/increment or decrement
+}
+
+// ======================================================Do while loop==========================================
+
+
+let score=11 //initialization
+
+do {
+    console.log(`score is ${score}`); //score is 11
+    score++//increment/decrement
+} while (score<=10);//condition checking
+
+
+//in the above it firstly execute once then start from checking conditions
