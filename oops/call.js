daf04a57c324ee39d7a66c97277dbdcb3f463ce6
sakshi <sakshikolekar2003@gmail.com>
2024-03-02 02:46:20 +0530

    call and this in js.

---
+++ oops/call.js
@@ -0,0 +1,16 @@
+//call stack 
+//ehen we call setusername method from createuername method.it will realease the value after execution 
+//so we cant get user name for our method ..so by using call we send it the this of our method to 
+//store its value before releasing its environment.
+function setUsername(username) {
+    this.username=username;
+      
+}
+function createUsername(userName,email,password) {
+    setUsername.call(this,userName)//call pass the current refence to another method to store its value
+    this.email=password
+    this.password=password
+}
+const me=new createUsername("ram","abc@123",113)
+console.log(me);//createUsername { email: 113, password: 113 } //before call
+console.log(me);// createUsername { username: 'ram', email: 113, password: 113 }//after call
